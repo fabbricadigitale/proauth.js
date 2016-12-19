10b7d151d341c1e13f8f6f2f469f37fc41383d2a
Leonardo Grasso <l.grasso@fabbricadigitale.it>
2016-12-19 18:24:12 +0100

    New: Multiple build targets

--- rollup.config.js
+++ rollup.config.js
@@ -8,17 +8,23 @@ function inDevelopment() {
   return process.env.BUILD_ENV && ['development', 'dev', 'develop'].indexOf(process.env.BUILD_ENV.toLowerCase()) >= 0
 }
 
+var target = (function(target) {
+  target = String(target).toLowerCase()
+  if (['client', 'legacy', 'service'].indexOf(target) < 0) throw Error('Invalid target')
+  return target
+})(process.env.BUILD_TARGET)
+
 var babelOpts = {
   presets: ['es2015-rollup'],
   exclude: 'node_modules/**'
 }
 
 var rollupOpts = {
-  entry: 'src/client.js',
+  entry: `src/${target}.js`,
   moduleName: 'proauth',
   format: 'umd',
   plugins: [ babel(babelOpts) ],
-  dest: 'dist/client.js'
+  dest: `dist/${target}.js`
 }
 
 if (inDevelopment()) {
