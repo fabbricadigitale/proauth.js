df0d504a5b6be3523a0bb427ab127ab544e66947
Leonardo Grasso <l.grasso@fabbricadigitale.it>
2016-12-19 19:04:31 +0100

    New: Legacy sub-package

---
+++ src/legacy/Worker.js
@@ -0,0 +1,42 @@
+export default class Worker {
+  constructor() {
+
+    // EventTarget
+    var delegate = document.createDocumentFragment();
+    [
+      'addEventListener',
+      'dispatchEvent',
+      'removeEventListener'
+    ].forEach(f =>
+      this[f] = (...xs) => delegate[f](...xs)
+      )
+  }
+
+  postMessage(aMessage, transferList) {
+
+    let e = new class extends CustomEvent {
+      constructor() {
+        super('message', {bubbles: false, cancelable: true})
+      }
+      get data() {
+        return aMessage
+      }
+      get ports() {
+        let ret = []
+        (transferList || []).forEach(function(transfer) {
+          if (transfer instanceof MessagePort) {
+            ret.push(transfer)
+          }
+        })
+        return ret.length > 0 ? ret : null
+      }
+    };
+
+    if (this.onmessage) {
+      this.onmessage(e);
+    }
+
+    this.dispatchEvent(e);
+  }
+
+}
