2bd92f5daf39b7ce416c9e047120b2b50c15faaf
Leonardo Grasso <l.grasso@fabbricadigitale.it>
2016-12-19 18:31:34 +0100

    Update: Disabling ES5 traspiling

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@
 
 /* global process */
 
-import babel from 'rollup-plugin-babel';
-
 function inDevelopment() {
   return process.env.BUILD_ENV && ['development', 'dev', 'develop'].indexOf(process.env.BUILD_ENV.toLowerCase()) >= 0
 }
@@ -14,16 +12,10 @@ var target = (function(target) {
   return target
 })(process.env.BUILD_TARGET)
 
-var babelOpts = {
-  presets: ['es2015-rollup'],
-  exclude: 'node_modules/**'
-}
-
 var rollupOpts = {
   entry: `src/${target}.js`,
   moduleName: 'proauth',
   format: 'umd',
-  plugins: [ babel(babelOpts) ],
   dest: `dist/${target}.js`
 }
 
