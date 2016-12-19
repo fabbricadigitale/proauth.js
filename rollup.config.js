5b1ac7ef47ed2029ef073ccefbd972bebccb357d
Leonardo Di Donato <l.didonato@fabbricadigitale.it>
2016-12-19 13:13:23 +0100

    Build: Fresh build system

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/* global process */
+
+import babel from 'rollup-plugin-babel';
+
+function inDevelopment() {
+  return process.env.BUILD_ENV && ['development', 'dev', 'develop'].indexOf(process.env.BUILD_ENV.toLowerCase()) >= 0
+}
+
+var babelOpts = {
+  presets: ['es2015-rollup'],
+  exclude: 'node_modules/**'
+}
+
+var rollupOpts = {
+  entry: 'index.m.js',
+  moduleName: 'proauth',
+  format: 'umd',
+  plugins: [ babel(babelOpts) ],
+  dest: 'index.js'
+}
+
+if (inDevelopment()) {
+  rollupOpts.sourceMap = 'inline'
+}
+
+export default rollupOpts
