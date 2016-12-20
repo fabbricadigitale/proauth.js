f91084c480bdfb0d704479cfe85dfd8c08dcfe0f
Leonardo Di Donato <l.didonato@fabbricadigitale.it>
2016-12-20 01:39:59 +0100

    Update: Create worker methods at prototype level
    This approach is slightly faster since the code is executed only once.
    Rather constructor code is executed every time a new instance is created.
    
    Serious advantage of prototype over constructor is that the former is compatible with class inheritance.

--- src/legacy/Worker.js
+++ src/legacy/Worker.js
@@ -1,29 +1,24 @@
 export default class Worker {
   constructor() {
-
     // EventTarget
     var delegate = document.createDocumentFragment();
     [
       'addEventListener',
       'dispatchEvent',
       'removeEventListener'
-    ].forEach(f =>
-      this[f] = (...xs) => delegate[f](...xs)
-      )
+    ].forEach(f => /*this[f]*/ Worker.prototype[f] = (...xs) => delegate[f](...xs))
   }
-
   postMessage(aMessage, transferList) {
-
     let e = new class extends CustomEvent {
       constructor() {
-        super('message', {bubbles: false, cancelable: true})
+        super('message', { bubbles: false, cancelable: true })
       }
       get data() {
         return aMessage
       }
       get ports() {
         let ret = []
-        (transferList || []).forEach(function(transfer) {
+        (transferList || []).forEach(transfer => {
           if (transfer instanceof MessagePort) {
             ret.push(transfer)
           }
@@ -31,11 +26,9 @@ export default class Worker {
         return ret.length > 0 ? ret : null
       }
     };
-
     if (this.onmessage) {
       this.onmessage(e);
     }
-
     this.dispatchEvent(e);
   }
 
