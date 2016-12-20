88fd6f5971958ac83803d747efea0866bb1f9afb
Leonardo Di Donato <leodidonato@gmail.com>
2016-12-20 01:07:10 +0100

    Build: Build system refinements

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,19 @@
 
 /* global process */
 
-function inDevelopment() {
-  return process.env.BUILD_ENV && ['development', 'dev', 'develop'].indexOf(process.env.BUILD_ENV.toLowerCase()) >= 0
-}
+const inDevelopment = () => process.env.BUILD_ENV && ['development', 'dev', 'develop'].indexOf(process.env.BUILD_ENV.toLowerCase()) >= 0
 
-var target = (function(target) {
+const target = (target => {
   target = String(target).toLowerCase()
   if (['client', 'legacy', 'service'].indexOf(target) < 0) throw Error('Invalid target')
   return target
 })(process.env.BUILD_TARGET)
 
-var rollupOpts = {
+const rollupOpts = {
   entry: `src/${target}.js`,
   moduleName: 'proauth',
   format: 'umd',
-  dest: `dist/${target}.js`
+  dest: `lib/${target}.js`
 }
 
 if (inDevelopment()) {
