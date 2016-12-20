01eb9ead2ae8e4aaa57d399696c31e10ccf42d50
Leonardo Grasso <l.grasso@fabbricadigitale.it>
2016-12-20 19:51:35 +0100

    Update: Communication protocol and session handling

--- src/service/OAuth2Handler.js
+++ src/service/OAuth2Handler.js
@@ -1,9 +1,22 @@
 export default class Oauth2Handler {
-  constructor(target, fetch) {
-    target.addEventListener('fetch', (event) => {
-      console.log(`hooking ${event.request.url}`, event);
-      event.respondWith(fetch(event.request));
-    });
+
+  /**
+   * @param {Object} config
+   * @param {SessionHandler} session
+   */
+  constructor (config, session) {
+    this.config = config
+    this.session = session
+  }
+
+  /**
+   * @param {FetchEvent} event
+   * @param {Function} fetch
+   */
+  handle (event, fetch) {
+    console.log(`Oauth2Handler::handle for ${event.request.url}`)
+    console.log('Config', this.config, 'Event', event)
+    event.respondWith(fetch(event.request));
   }
 
 }
