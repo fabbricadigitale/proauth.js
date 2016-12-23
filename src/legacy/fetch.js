fd4b9ac874e7a072e1697fa5858bbf70c3e3ee03
Leonardo Grasso <l.grasso@fabbricadigitale.it>
2016-12-23 21:41:13 +0100

    Fix: Promise polyfill does not absolutize URLs

--- src/legacy/fetch.js
+++ src/legacy/fetch.js
@@ -1,9 +1,16 @@
+import absolutePath from '../common/absolutePath'
 let originalFetch = window.fetch
 
 let fetch = function(input, init) {
   let self = this
   return new Promise((resolve, reject) => {
     let request = new Request(input, init);
+
+    if (originalFetch.polyfill) {
+      // fetch polyfill does not absolutize the url, so...
+      request.url = absolutePath(request.url)
+    }
+
     let fetchEvent = new CustomEvent('fetch');
     fetchEvent.request = request
     fetchEvent.respondWith = resolve
