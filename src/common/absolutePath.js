1168f2b47460603fa151e156cff4da4ffd3b8917
Leonardo Di Donato <leodidonato@gmail.com>
2016-12-25 23:18:32 +0100

    Update: Use string templates rather than string concat

--- src/common/absolutePath.js
+++ src/common/absolutePath.js
@@ -1,5 +1,5 @@
 export default href => {
   var link = document.createElement("a")
   link.href = href
-  return (link.protocol + "//" + link.host + link.pathname + link.search + link.hash)
+  return `${link.protocol}//${link.host}${link.pathname}${link.search}${link.hash}`
 }
