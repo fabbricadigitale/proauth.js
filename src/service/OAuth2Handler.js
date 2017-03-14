aab2c224f486c5c702776381d7ced05d269dbb99
Leonardo Grasso <l.grasso@fabbricadigitale.it>
2017-03-14 17:12:33 +0100

    Update: Improve Oauth2Handler code

--- src/service/OAuth2Handler.js
+++ src/service/OAuth2Handler.js
@@ -1,5 +1,24 @@
 import OAuth2Client from "../common/OAuth2Client"
 
+const attachAuthorization = (session, request) => {
+  const tokens = session.content || {}
+  if (tokens.accessToken) {
+    request.headers.set("Authorization", `${tokens.tokenType} ${tokens.accessToken}`)
+  }
+}
+
+const updateSession = (session, grantPromise) => {
+  return grantPromise.then((oA2Response) => {
+    console.log("proauth.js: authentication successful, updating session", oA2Response)
+    session.content = oA2Response.toObject()
+    return true
+  }, (oA2Error) => {
+    console.log("proauth.js: cannot authenticate, clearing session", oA2Error)
+    session.clear()
+    return false
+  })
+}
+
 export default class Oauth2Handler {
 
   /**
@@ -14,68 +33,72 @@ export default class Oauth2Handler {
     this.session = session
     this.fetch = fetch
     this.oauth2Client = new OAuth2Client(settings.oauthUrl, settings.oauthClientId, fetch)
+    this.invalidGrantHttpCode = 401
   }
 
   /**
+   * Update the session on response
+   *
+   * @param {FetchEvent} event The intercepted event
+   * @returns {void}
+   */
+  handleAuthentication(event) {
+    const fetch = this.fetch
+    event.respondWith(fetch(event.request).then((response) => {
+      // Just update the session on response
+      return updateSession(
+        this.session,
+        this.oauth2Client.handleAuthenticationResponse(response.clone())
+      ).then(() => response)
+    }))
+  }
+
+  /**
+   * Set the current token and handle 401s
+   *
    * @param {FetchEvent} event The intercepted event
    * @returns {void}
    */
   handle(event) {
     console.log(`proauth.js Oauth2Handler::handle for ${event.request.url}`)
 
-    const {oauthUrl} = this.settings
+    const { oauthUrl } = this.settings
     const request = event.request
     const url = request.url
     const isAuthRequest = url === oauthUrl || `${url}/` === oauthUrl
+
+    if (isAuthRequest) {
+      this.handleAuthentication(event)
+      return
+    }
+
+    // Attach the current token
+    attachAuthorization(this.session, request)
+
     const oauth = this.oauth2Client
-    const invalidGrantHttpCode = 401
-    let tokens = this.session.content || {}
-
-    const updateSession = (grantPromise) => {
-      return grantPromise.then((oA2Response) => {
-        console.log("proauth.js: authentication successful, updating session", oA2Response)
-        tokens = this.session.content = oA2Response.toObject()
-        return true
-      }, (oA2Error) => {
-        console.log("proauth.js: cannot authenticate, clearing session", oA2Error)
-        this.session.clear()
-        return false
-      })
-    }
-
-    // If it is not trying to authenticate then attach the access token
-    if (!isAuthRequest && tokens.accessToken) {
-      request.headers.set("Authorization", `${tokens.tokenType} ${tokens.accessToken}`)
-    }
-
     const requestCopy = request.clone()
-    // Fetch and handle the response
     const fetch = this.fetch
+
+    // Finally, fetch and handle the response
     event.respondWith(fetch(request).then((response) => {
-
-      if (isAuthRequest) {
-        return updateSession(
-          oauth.handleAuthenticationResponse(response.clone())
-        ).then(() => response)
-      }
-
-      if (response.status === invalidGrantHttpCode && tokens.refreshToken) {
+      const tokens = this.session.content || {}
+      if (response.status === this.invalidGrantHttpCode && tokens.refreshToken) {
         // Got 401, but we have a refresh token, so try to get a new access token
         return oauth.refreshToken(tokens.refreshToken).then((authResponse) => {
           return updateSession(
+            this.session,
             oauth.handleAuthenticationResponse(authResponse)
           ).then((newSession) => {
             // We got a new token?
             // Then try to re-fetch the original request...
             if (newSession) {
-              requestCopy.headers.set("Authorization", `${tokens.tokenType} ${tokens.accessToken}`)
+              attachAuthorization(this.session, requestCopy)
               return fetch(requestCopy)
             } //...else passthrough the 401 response
             return response
           })
         })
       }
-
       return response // Passthrough any others
     }))
   }
