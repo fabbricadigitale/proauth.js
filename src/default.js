428b5a8b2542d16b8e9c7b7acd8f205c322a0397
Lorenzo Cesana <l.cesana@fabbricadigitale.it>
2017-06-12 12:59:05 +0200

    Fix: Make service worker work also during first page load

--- src/default.js
+++ src/default.js
@@ -1,13 +1,38 @@
 const client = proauth.client
 
-navigator.serviceWorker.register("/service-worker.js")
+const setServiceWorker = (sw) => {
+  client.serviceWorker = sw
+  client.settings.legacyMode = false
+}
 
-  .then(() => {
-    return navigator.serviceWorker.ready
-  })
-  .then(() => {
-    client.serviceWorker = navigator.serviceWorker
-    client.settings.legacyMode = false
-  })
+if (navigator.serviceWorker) {
+
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(() => {
+      return navigator.serviceWorker.ready
+    })
+    .then((registration) => {
+      // SW is ready, but its state can be either activating or activated.
+      // We must wait until it's activated, so the clients will be claimed.
+
+      console.log(registration.active, registration.active.state)
+
+      if (registration.active.state === "activated") {
+
+        setServiceWorker(navigator.serviceWorker)
+
+      } else {
+
+        registration.active.addEventListener("statechange", () => {
+          if (registration.active.state === "activated") {
+
+            setServiceWorker(navigator.serviceWorker)
+
+          }
+        })
+
+      }
+    })
+}
 
 export default { client }
