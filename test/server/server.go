a2784aadbcb48244fdf8281ece56df4a429ce79c
Leonardo Grasso <me@leonardograsso.com>
2017-06-14 16:13:17 +0200

    Update: Ability to shutdown test server

--- test/server/server.go
+++ test/server/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -51,7 +52,7 @@ func ReturnAuthorizationHeader(w http.ResponseWriter, r *http.Request, _ httprou
 func ReturnSomeHeaders(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-  // Header purposely not in alphabetical order and upper/lowercased
+	// Header purposely not in alphabetical order and upper/lowercased
 	w.Header().Set("Key-A", "Value-A")
 	w.Header().Set("Key-B", "Value-B")
 	w.Header().Set("Key-c", "Value-c")
@@ -97,6 +98,10 @@ func AllowCors(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "")
 }
 
+func Shutdown(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	os.Exit(0)
+}
+
 func main() {
 	router := httprouter.New()
 
@@ -120,6 +125,8 @@ func main() {
 
 	router.OPTIONS("/return-401", AllowCors)
 	router.GET("/return-401", Return401)
+
+	router.POST("/shutdown", Shutdown)
 
 	log.Fatal(http.ListenAndServe(":8060", router))
 }
