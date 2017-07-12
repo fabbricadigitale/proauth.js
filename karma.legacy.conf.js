76dacfb22d5827691187711d5df0f533b34c4148
Leonardo Grasso <me@leonardograsso.com>
2017-07-12 18:22:00 +0200

    Update: Refactor SauceLabs configuration

--- karma.legacy.conf.js
+++ karma.legacy.conf.js
@@ -1,8 +1,8 @@
 "use strict"
 
-const sharedConf = require("./karma.shared.conf.js")
+const sharedOpts = require("./karma-options.js")
 
-const browsers = process.env.TEST_SAUCELABS ? [
+const sauceBrowsers =  [
   "sl_chrome_latest_winXP",
   "sl_chrome_latest_win7",
   "sl_chrome_latest_win8",
@@ -21,39 +21,41 @@ const browsers = process.env.TEST_SAUCELABS ? [
 
   "sl_chrome_latest_android6_0",
   "sl_chrome_latest_android7_0"
-] : ["Chrome", "Firefox"]
+]
+
+const options = {
+  files: [
+        "test/fixture/proauth-settings.js",
+        "test/fixture/tests-settings.js",
+        "lib/legacy.js",
+        "test/**/*.js"
+      ],
+
+  preprocessors: {
+    "lib/legacy.js": "coverage"
+  },
+
+  coverageReporter: {
+    dir: "coverage",
+    reporters: [
+      { type: "json", subdir: ".", file: "legacy.coverage.json" },
+      { type: "lcovonly", subdir: ".", file: "legacy.lcov.info" },
+      { type: "text", subdir: ".", file: "legacy.text.txt" },
+      { type: "text-summary", subdir: ".", file: "legacy.text.summary.txt" },
+      { type: "html", subdir: "html/legacy" }
+    ]
+  }
+}
 
 module.exports = function (config) {
-  sharedConf(config)
+  config.set(sharedOpts)
 
-  config.set({
+  config.set(options)
 
-    files: [
-      "test/fixture/proauth-settings.js",
-      "test/fixture/tests-settings.js",
-      "lib/legacy.js",
-      "test/**/*.js"
-    ],
-
-    preprocessors: {
-      "lib/legacy.js": "coverage"
-    },
-
-    coverageReporter: {
-      dir: "coverage",
-      reporters: [
-        { type: "json", subdir: ".", file: "legacy.coverage.json" },
-        { type: "lcovonly", subdir: ".", file: "legacy.lcov.info" },
-        { type: "text", subdir: ".", file: "legacy.text.txt" },
-        { type: "text-summary", subdir: ".", file: "legacy.text.summary.txt" },
-        { type: "html", subdir: "html/legacy" }
-      ]
-    },
-
-    exclude: [
-    ],
-
-    browsers
-
-  })
+  if (process.env.TEST_SAUCELABS) {
+    config.set({
+      sauceLabs: Object.assign({}, sharedOpts.sauceLabs, { testName: "proauth.js (legacy)" }),
+      browsers: sauceBrowsers
+    })
+  }
 }
